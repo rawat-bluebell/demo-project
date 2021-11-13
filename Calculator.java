2ba4c78c320a26b204c19671859ab6e718617fd4
rawat_surat <90523745+rawat-bluebell@users.noreply.github.com>
2021-11-13 05:46:37 +0530

    Update

--- Calculator.java
+++ Calculator.java
@@ -4,16 +4,15 @@ public class Calculator{
 	}
 	int multiply(int num1, int mum2){
 	  return num1*num2;
-	}
-	
-<<<<<<< HEAD
+	}	
+
 	int division(int int1, int int2)
 	{
 		return int1 / int2;
-=======
+
 	int substraction(int int1, int int2){
 		return int1 - int2
->>>>>>> f57097b8379e349c2b91a35f0a11bcbca16e23f5
+
 	}
 
 }
