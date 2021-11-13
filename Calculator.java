7ac97b578caaa667fd18ee7bf02b25ff9bebd4fa
SuratRawat <rawat.surat@gmail.com>
2021-11-13 10:47:33 +0530

    final commit

--- Calculator.java
+++ Calculator.java
@@ -1,23 +1,21 @@
 public class Calculator{
-	// Point 1
+	// Point 1 Addition
 	int sum(int num1, int num2){
 		return num1+num2;
 	}
-<<<<<<< HEAD
-	//Point 2
-=======
-	// Multiply
->>>>>>> 7bb0c425155a50c57117fdd03f274b4120b3af72
+	//Point 2 multiplication
 	int multiply(int num1, int mum2){
 	  return num1*num2;
 	}	
-
+	//Point 3 Division
 	int division(int int1, int int2)
 	{
 		return int1 / int2;
-
+	}
+	
+	//Point 4 Substraction
 	int substraction(int int1, int int2){
-		return int1 - int2
+		return int1 - int2;
 
 	}
 
