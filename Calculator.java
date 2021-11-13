1a5545c2395ed7ede233541be19468f7a0389574
Ubantu_bluebell <xyz@abc.com>
2021-11-13 06:05:36 +0530

    commit by Ubantu

--- Calculator.java
+++ Calculator.java
@@ -5,11 +5,11 @@ public class Calculator{
 	int multiply(int num1, int mum2){
 	  return num1*num2;
 	}	
-
+	// Commnet Division
 	int division(int int1, int int2)
 	{
 		return int1 / int2;
-
+	// Commnet Sub
 	int substraction(int int1, int int2){
 		return int1 - int2
 
